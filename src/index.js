35cdca78c8e79a889c385dd4099ae3683d93f715
pranayzv <pranayzvlogs@gmail.com>
2022-05-02 16:07:19 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="https://pranayzv.github.io/JWT-TokenValidation/">
     <App />
   </BrowserRouter>
 );
